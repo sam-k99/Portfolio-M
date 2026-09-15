32caa28a92887cb5dcda80150a04129285943f10
sam-k99 <rugalass@tutamail.com>
2026-09-15 15:37:15 +0530

    perf: optimized small functions

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,10 +34,9 @@ const Hero = () => {
             />
           </div>
           <div className="flex h-full flex-col justify-center gap-2 sm:gap-3">
-            <h1 className="-ml-4 flex flex-col items-center sm:flex-row sm:items-center gap-0 sm:gap-2 text-2xl font-normal tracking-tight text-foreground sm:text-2xl md:text-3xl">
-              <span>Sameer</span>
-              <span className="flex items-center gap-2 whitespace-nowrap">
-                Khan
+            <h1 className="flex items-center gap-2 text-2xl font-normal tracking-tight text-foreground sm:text-2xl md:text-3xl">
+              Sameer Khan
+              <span className="text-background shrink-0">
                 <BadgeCheck color="currentColor" fill="#3b82f6" size={24} />
               </span>
             </h1>
